9a8eee34b47acdc8629bffa49b3b2c054102586a
JustJaxttv <156222998+JustJaxttv@users.noreply.github.com>
2024-01-17 12:17:46 -0600

    1

---
+++ java.js
@@ -0,0 +1,11 @@
+// Kobe Swier 1/17/24
+
+var value = 5; 
+
+value = value * 3;
+
+let promtval= prompt("Please Enter Your Favorite Color");
+
+console.log("value: " + value + "\nPromtval: " + promtval);
+
+
