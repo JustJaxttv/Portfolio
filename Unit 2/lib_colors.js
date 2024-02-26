c0c6689412b3dcd8a5e3c8d02713be363fa61bda
JustJaxttv <156222998+JustJaxttv@users.noreply.github.com>
2024-02-25 19:42:51 -0600

    2/25

---
+++ Unit 2/lib_colors.js
@@ -0,0 +1,13 @@
+ /* Kobe Swier
+  * 2/25/24
+  */
+ 
+ class ColorClass {
+    constructor(name) {
+        this.name = name;
+    } 
+
+    logNameMethod() {
+        console.log(this.name);
+    }
+ }
---
+++ Unit 2/objects.js
@@ -0,0 +1,11 @@
+/* Kobe Swier
+ * 2/25/24
+ */
+
+let red = new ColorClass("Red"); 
+let blue = new ColorClass("Blue");
+let yellow = new ColorClass("Yellow");
+
+red.logNameMethod();
+blue.logNameMethod();
+yellow.logNameMethod();
