f1f3c0b0fb00e3b0a1fd3dcfc2f5a765b1244a56
JustJaxttv <156222998+JustJaxttv@users.noreply.github.com>
2024-03-17 22:40:55 -0500

    3/17

---
+++ Unit 2/async.js
@@ -0,0 +1,29 @@
+async function race() {
+    let runner1 = false;
+    let runner2 = false;
+    let loser = '';
+
+    let runner1Go = new Promise((resolve) => {
+        setTimeout(() => {
+            loser = 'runner1';
+            resolve(true);
+        }, 3000);
+    });
+    
+    let runner2Go = new Promise((resolve) => {
+        setTimeout(() => {
+            loser = 'runner2';
+            resolve(true);
+        }, 7000);
+    });
+}
+
+return [
+    await runner1Go,
+    await runner2Go,
+    loser
+];
+
+race().then((result) => {
+    console.log("Loser:", result[2]);
+});
