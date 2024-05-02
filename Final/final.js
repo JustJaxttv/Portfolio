3d50b898f4a44f49483b350b4faa2de849635060
JustJaxttv <kobe5105@outlook.com>
2024-05-02 02:46:01 -0500

    5/2

--- Final/final.js
+++ Final/final.js
@@ -6,3 +6,76 @@ function calculateSquare() {
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "The square of " + inputNumber + " is " + square;
   }
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+
+    insert(data) {
+      let newNode = new Node(data);
+      if (!this.head) {
+        this.head = newNode;
+      } else {
+        let current = this.head;
+        while (current.next) {
+          current = current.next;
+        }
+        current.next = newNode;
+      }
+      this.display();
+    }
+
+    search(data) {
+        let current = this.head;
+        while (current) {
+          if (current.data === data) {
+            return true;
+          }
+          current = current.next;
+        }
+        return false;
+      }
+
+      display() {
+        let current = this.head;
+        let listString = "";
+        while (current) {
+          listString += current.data + " -> ";
+          current = current.next;
+        }
+        listString += "null";
+        document.getElementById("listDisplay").innerText = listString;
+      }
+    }
+
+    let linkedList = new LinkedList();
+
+    function insertNode() {
+      let insertValue = document.getElementById("insertValue").value;
+      if (insertValue.trim() !== "") {
+        linkedList.insert(insertValue);
+      } else {
+        alert("Please enter a valid value to insert.");
+      }
+    }
+
+    function searchNode() {
+        let searchValue = document.getElementById("searchValue").value;
+        if (searchValue.trim() !== "") {
+          if (linkedList.search(searchValue)) {
+            alert("Value " + searchValue + " found in the list.");
+          } else {
+            alert("Value " + searchValue + " not found in the list.");
+          }
+        } else {
+          alert("Please enter a valid value to search.");
+        }
+      }
